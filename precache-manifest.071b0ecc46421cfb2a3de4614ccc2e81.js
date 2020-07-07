d4e0e8a20dcf853e61353162084f8d1b063b752d
claytonfaria <claytonfaria@gmail.com>
2020-07-08 06:35:04 +0800

    Updates

---
+++ precache-manifest.071b0ecc46421cfb2a3de4614ccc2e81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a0ef81fc64fa48a62fa1078b18283b",
+    "url": "/claytonfaria/COVID19/index.html"
+  },
+  {
+    "revision": "9a37ddbd558d40b6a686",
+    "url": "/claytonfaria/COVID19/static/css/main.0daaf817.chunk.css"
+  },
+  {
+    "revision": "086057aeb15b2633e35f",
+    "url": "/claytonfaria/COVID19/static/js/2.84290951.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/claytonfaria/COVID19/static/js/2.84290951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a37ddbd558d40b6a686",
+    "url": "/claytonfaria/COVID19/static/js/main.453740dd.chunk.js"
+  },
+  {
+    "revision": "c9aeff6dff6d80a56ecf",
+    "url": "/claytonfaria/COVID19/static/js/runtime-main.be7806cc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/claytonfaria/COVID19/static/media/image.d7265326.png"
+  }
+]);
